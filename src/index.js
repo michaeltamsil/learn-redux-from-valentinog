0fb7aee14906755b537ea3adde75ae1a63121212
michael tamsil <michaeltamsil@gmail.com>
2019-03-27 11:03:03 +0700

    learn connect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './components/App.jsx';
+
+// if you're in create-react-app import the files as:
+// import store from "./js/store/index";
+// import App from "./js/components/App.jsx";
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
 import index from './js';
